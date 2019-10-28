c4a97c3b00996b0b0c41d4f54a11e936ff987943
Wesley Rosenblum <wesleyr@amazon.com>
2019-10-28 16:58:14 -0700

    *Issue #, if available:* #108
    *Description of changes:*
    
    Add a basic example for encrypting and decrypting with a KMS CMK.
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.
    
    # Check any applicable:
    - [ ] Were any files moved? Moving files changes their URL, which breaks all hyperlinks to the files.

--- src/test/java/com/amazonaws/encryptionsdk/kms/KMSTestFixtures.java
+++ src/test/java/com/amazonaws/encryptionsdk/kms/KMSTestFixtures.java
@@ -1,6 +1,19 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except
+ * in compliance with the License. A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package com.amazonaws.encryptionsdk.kms;
 
-final class KMSTestFixtures {
+final public class KMSTestFixtures {
     private KMSTestFixtures() {
         throw new UnsupportedOperationException(
                 "This class exists to hold static constants and cannot be instantiated."
@@ -14,7 +27,7 @@ final class KMSTestFixtures {
      * This should go without saying, but never use these keys for production purposes (as anyone in the world can
      * decrypt data encrypted using them).
      */
-    static final String[] TEST_KEY_IDS = new String[] {
+    public static final String[] TEST_KEY_IDS = new String[] {
             "arn:aws:kms:us-west-2:658956600833:key/b3537ef1-d8dc-4780-9f5a-55776cbb2f7f",
             "arn:aws:kms:eu-central-1:658956600833:key/75414c93-5285-4b57-99c9-30c1cf0a22c2"
     };
