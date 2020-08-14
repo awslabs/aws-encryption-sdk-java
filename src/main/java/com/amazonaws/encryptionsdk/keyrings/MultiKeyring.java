6a73f6ac66f08ee2406f7fd766b4f65fc04275cb
Valerie Lambert <lavaleri@amazon.com>
2020-08-13 19:00:07 -0700

    refactor!: Update multikeyring generator to require generating keys
    BREAKING CHANGE: The Multi-Keyring will fail OnEncrypt if configured
    with a generator and the input materials contain a plaintext data key.

--- src/main/java/com/amazonaws/encryptionsdk/keyrings/MultiKeyring.java
+++ src/main/java/com/amazonaws/encryptionsdk/keyrings/MultiKeyring.java
@@ -50,6 +50,12 @@ class MultiKeyring implements Keyring {
 
         EncryptionMaterials resultMaterials = encryptionMaterials;
 
+        // If a generator keyring is configured, it MUST be used to generate the cleartext data key
+        if (generatorKeyring != null && resultMaterials.hasCleartextDataKey()) {
+            throw new AwsCryptoException(
+                    "A generator was configured but the keyring recieved materials already containing a cleartext data key");
+        }
+
         if (generatorKeyring != null) {
             resultMaterials = generatorKeyring.onEncrypt(encryptionMaterials);
         }
